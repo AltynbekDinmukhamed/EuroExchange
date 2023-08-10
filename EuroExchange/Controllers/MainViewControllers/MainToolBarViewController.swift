1175aaafb9585ea824bdb86329673f46ff4bef38
AltynbekDinmukhamed <a.dimash.a_03@mail.ru>
2023-08-10 16:08:15 +0600

    Main tool bar set up

--- EuroExchange/Controllers/MainViewControllers/MainToolBarViewController.swift
+++ EuroExchange/Controllers/MainViewControllers/MainToolBarViewController.swift
@@ -11,21 +11,35 @@ import UIKit
 class MainToolBarViewController: UITabBarController {
     //MARK: -variable-
     
+    
     //MARK: -LifeCycle-
     override func viewDidLoad() {
         super.viewDidLoad()
+        navigationController?.navigationBar.isHidden = true
         generateToolBar()
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        navigationController?.navigationBar.isHidden = true
     }
     
     //MARK: -Functions-
     private func generateToolBar() {
-        viewControllers = [
-            
-        ]
+        let firstViews = createNav(with: "Home", and: UIImage(systemName: "house"), vc: AddNewCardViewController())
+        let secoundView = createNav(with: "Exchange", and: UIImage(named: "Exhangelogo"), vc: ExchangeCurrency())
+        let thirdView = createNav(with: "Card info", and: UIImage(systemName: "house"), vc: CardInfoViewController())
+        
+        setViewControllers([firstViews, secoundView, thirdView], animated: true)
     }
     
-    private func generanteVc(viewController: UIViewController, image: UIImage?) -> UIViewController {
-        viewController.tabBarItem.image = image
-        return viewController
+    private func createNav(with title: String, and image: UIImage?, vc: UIViewController) -> UINavigationController {
+        let nav = UINavigationController(rootViewController: vc)
+        
+        nav.tabBarItem.image = image
+        
+        nav.viewControllers.first?.navigationItem.rightBarButtonItem = UIBarButtonItem(title: "Button", style: .plain, target: self, action: nil)
+        
+        return nav
     }
 }
